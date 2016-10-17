99c2b06eb2bf78d21d16e0c16cb28ce651876461
oldchevy <nick.olszowy@gmail.com>
2016-10-16 19:04:44 -0700

    (feat) Add new problem, waterBlocks

---
+++ waterBlocks/waterBlocks.js
@@ -0,0 +1,104 @@
+/*
+Water Blocks
+
+You are given an input array where each element represents the height of a line of towers. 
+
+The width of every tower is 1.
+It starts raining. How much water is collected between the towers? 
+Eg. Input: [5, 2, 3, 2, 1, 3]
+Output: 4
+Visualization:
+
+'-' is water
+ '#' is a block
+
+#
+#
+# - # -  - #
+# # # # - #
+# # # # # #
+
+*/
+
+var waterBlocks = function (blocks) {
+  // TODO: Implement
+  
+  //I want to try building up aread where water can go
+  
+  var tempBegin = {
+    location: 0,
+    height: blocks[0]
+  };
+  
+  var tempEnd = {
+    location: 1,
+    height: blocks[1]
+  };
+
+  var ranges = [];
+  var result = 0;
+  
+  blocks.forEach((height, index) => {
+    
+    //Two options at each place, a height is less or greater than our temp begin
+    if (height > tempBegin.height) {
+      
+      //if we get a new highest thing, push the range into our valid places
+      ranges.push({
+        start: tempBegin.location,
+        end: index,
+        maxHeight: tempBegin.height
+      });
+      
+      //reassign tempBegin
+      tempBegin.location = index;
+      tempBegin.height = height;
+      
+      
+    } else {
+      
+      //reassign tempEnd as needed
+      if (height >= tempEnd.height) {
+        tempEnd.location = index;
+        tempEnd.height = height;
+        
+        //if we get here, we know we need to push the range and also reset both temps
+        ranges.push({
+          start: tempBegin.location,
+          end: tempEnd.location,
+          maxHeight: tempEnd.height
+        });
+        
+        tempBegin.location = index;
+        tempBegin.height = height;
+        tempEnd.location = index + 1;
+        tempEnd.height = blocks[index + 1];
+      }
+      
+      if (index === blocks.length - 1) {
+        ranges.push({
+          start: tempBegin.location,
+          end: index,
+          maxHeight: height
+        });
+      }
+      //assign a final range at to the end of the array
+    }
+  });
+  
+  console.log(ranges);
+  ranges.forEach(range => {
+    for (var i = range.start; i <= range.end; i++) {
+      if (blocks[i] < range.maxHeight) {
+        result += (range.maxHeight - blocks[i]);
+      }
+      console.log(range, result);
+    }
+  });
+  
+  return result;
+};
+
+module.exports = waterBlocks;
+
+waterBlocks([5, 2, 3, 2, 1, 3]);
---
+++ waterBlocks/waterBlocks.test.js
@@ -0,0 +1,40 @@
+//Test suite for findPivot
+var expect = require('chai').expect;
+var findPivot = require('./findPivot');
+
+describe('Find Pivot', function() {
+
+  var testArrays = [
+    { arr: [5, 2, 3, 2, 1, 3],
+      answer: 4 },
+    { arr: [2, 0, 3],
+      answer: 2 },
+    { arr: [3, 0, 0, 3],
+      answer: 6 },
+    { arr: [4, 1, 2, 3], 
+      answer: 3 },
+    { arr: [3, 2, 1, 4],
+      answer: 3 },
+    { arr: [3, 1, 4, 2, 5], 
+      answer: 4 },
+    { arr: [4, 1, 3, 1, 4], 
+      answer: 7 },
+    { arr: [3, 1, 2, 4, 3, 2, 1], 
+      answer: 3 },
+    { arr: [3, 1, 4, 2, 5], 
+      answer: 4 },
+    { arr: [1, 4, 1, 3, 1, 5, 3, 1, 4, 3, 1, 2, 3, 2, 2, 3, 4], 
+      answer: 23 },
+    { arr: [1000, 2, 5, 10000], 
+      answer: 1993 },
+
+  ];
+
+
+  testArrays.forEach(function(test) {
+    it('should find the pivot for ' + JSON.stringify(test.arr), function() {
+      expect(findPivot(test.arr)).to.equal(test.answer);
+    });
+  });
+
+});
