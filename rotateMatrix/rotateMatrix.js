06f022196f504293d0e3c65cdd20b1cb29c014ef
oldchevy <nick.olszowy@gmail.com>
2016-11-09 19:05:36 -0800

    Adds rotateMatrix problem

---
+++ rotateMatrix/rotateMatrix.js
@@ -0,0 +1,28 @@
+//Write a function that rotates a matrix 90 degrees (either direction, don't matter to me)
+//Constraints --> your goal is to do this in constant space and no more than one visit per entry
+
+
+//Ex: 
+//  [[1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16]]
+
+//Will become 
+//  [[13, 9, 5, 1],
+//   [14, 10, 6, 2],
+//   [15, 11, 7, 3],
+//   [16, 12, 8, 4]
+
+var rotateMatrix = function(matrix) {
+  //Todo: implement me!
+};
+
+var matrix = [[1,  2,  3,  4], 
+              [5,  6,  7,  8], 
+              [9,  10, 11, 12]];
+
+console.log(rotateMatrix(matrix));
+
+
+
