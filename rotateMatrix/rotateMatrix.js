30f70e98d49ab2b6f86560fcc1db29e584d4a0c8
oldchevy <nick.olszowy@gmail.com>
2016-11-10 23:50:54 -0800

    (add) Tests for rotateMatrix, update var names in waterBlocks

--- rotateMatrix/rotateMatrix.js
+++ rotateMatrix/rotateMatrix.js
@@ -18,11 +18,7 @@ var rotateMatrix = function(matrix) {
   //Todo: implement me!
 };
 
-var matrix = [[1,  2,  3,  4], 
-              [5,  6,  7,  8], 
-              [9,  10, 11, 12]];
-
-console.log(rotateMatrix(matrix));
+module.exports = rotateMatrix;
 
 
 
